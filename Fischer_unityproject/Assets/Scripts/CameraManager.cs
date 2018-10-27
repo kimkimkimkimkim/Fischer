c78e972f4f7324f3824bb0bde84f4f7aabcfb6fd
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2018-10-28 03:10:58 +0900

    メインルームの作成、カメラの移動

---
+++ Fischer_unityproject/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	private float angle = 0; //回転角
+	private float speed = 3f; //回転速度
+	private float dis = 7; //回転半径
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate(){
+		angle += Time.deltaTime * speed;
+		updateAngle();
+		updatePos();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void updateAngle(){
+		this.transform.localRotation = Quaternion.Euler(21f,angle,0f);
+	}
+
+	private void updatePos(){
+		float sin = (float)Math.Sin(angle * (Math.PI / 180));
+		float cos = (float)Math.Cos(angle * (Math.PI / 180));
+		this.transform.localPosition = new Vector3(-1*dis*sin,3,-1*dis*cos);
+	}
+
+}
