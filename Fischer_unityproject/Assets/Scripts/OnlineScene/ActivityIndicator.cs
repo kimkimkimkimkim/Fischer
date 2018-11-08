da7bbfbde80570e07cff095e1ad77ef8b37d1971
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2018-11-09 01:31:54 +0900

    【機能追加】マッチング機能追加
    ・マッチング開始したら部屋を検索
    ・空いてる部屋があればそこに参加
    ・空いてる部屋がなければ部屋を作成し待機

---
+++ Fischer_unityproject/Assets/Scripts/OnlineScene/ActivityIndicator.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// iOSやAndroidでインジケーターを表示するためのクラス
+/// Unity上では表示されない
+/// </summary>
+public class ActivityIndicator : MonoBehaviour {
+
+    //インジケーターの種類をインスペクターで設定する用
+    //ビルド時にエラーが出ないように、かつ、常にインスペクターに両方表示するように#ifを追加
+    #if UNITY_EDITOR || UNITY_IOS
+        [SerializeField]
+        private UnityEngine.iOS.ActivityIndicatorStyle _iosStyle;
+    #endif
+    #if UNITY_EDITOR || UNITY_ANDROID
+        [SerializeField]
+        private AndroidActivityIndicatorStyle _androidStyle;
+    #endif
+
+    //=================================================================================
+    //初期化
+    //=================================================================================
+
+    private void Awake (){
+        #if UNITY_IPHONE
+            Handheld.SetActivityIndicatorStyle(_iosStyle);
+        #elif UNITY_ANDROID
+            Handheld.SetActivityIndicatorStyle(_androidStyle);
+        #endif
+    }
+
+    //=================================================================================
+    //表示の切り替え
+    //=================================================================================
+
+    /// <summary>
+    /// インジケーターの表示
+    /// </summary>
+    public void Show(){
+        //フレームの最後にインジケーターのアニメーションが始まるので、フレームの終わりまで待つ
+        StartCoroutine(StartActivityIndicator());
+    }
+
+    private IEnumerator StartActivityIndicator(){
+        Handheld.StartActivityIndicator();
+        yield return new WaitForSeconds(0);
+    }
+
+    /// <summary>
+    /// インジケーター非表示
+    /// </summary>
+    public void Hide(){
+        Handheld.StopActivityIndicator ();
+    }
+
+}
---
+++ Fischer_unityproject/Assets/Scripts/OnlineScene/MachingManager.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class MachingManager : MonoBehaviour {
+
+	private GameObject indicator;
+	private DatabaseReference reference;
+	private bool isLastRoom = true;
+
+	// Use this for initialization
+	void Start () {
+		/*
+		 * Firebase関連
+		 */
+		// Set up the Editor before calling into the realtime database.
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://fischer-66e26.firebaseio.com");
+		// Get the root reference location of the database.
+		reference = FirebaseDatabase.DefaultInstance.RootReference;
+		Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+  		var dependencyStatus = task.Result;
+  		if (dependencyStatus == Firebase.DependencyStatus.Available) {
+		    // Create and hold a reference to your FirebaseApp, i.e.
+		    //   app = Firebase.FirebaseApp.DefaultInstance;
+		    // where app is a Firebase.FirebaseApp property of your application class.
+
+		    // Set a flag here indicating that Firebase is ready to use by your
+		    // application.
+		  } else {
+		    UnityEngine.Debug.LogError(System.String.Format(
+		      "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+		    // Firebase Unity SDK is not safe to use here.
+		  }
+		});
+
+		//オブジェクトの参照
+		referenceObject();
+
+		//インジケーターの表示
+		indicator.GetComponent<ActivityIndicator>().Show();
+
+		//マッチング開始
+		searchRoom();
+		
+	}
+
+	//オブジェクトの参照
+	private void referenceObject(){
+		indicator = GameObject.Find("ActivityIndicator");
+	}
+
+	//部屋の検索
+	private void searchRoom(){
+		//部屋の中に一人しかいない時OK
+		FirebaseDatabase.DefaultInstance
+		.GetReference("onlineroom")
+		.GetValueAsync().ContinueWith(task => {
+			if (task.IsFaulted) {
+				
+			}
+			else if (task.IsCompleted) {
+				DataSnapshot snapshot = task.Result;
+				IEnumerator<DataSnapshot> en = snapshot.Children.GetEnumerator();;
+
+				while (en.MoveNext()) {
+					DataSnapshot data = en.Current;
+					/* 
+					string name = (string)data.Child("name").Value;
+					int score   = (int)(long)data.Child("score").Value;
+					*/
+					if(data.ChildrenCount == 1){
+						//部屋に一人しかいない
+						intoTheRoom(int.Parse(data.Key));
+						Debug.Log(data.Key + "の部屋に参加");
+						isLastRoom = false;
+						break;
+					}
+				}
+
+				//空いてる部屋がない
+				if(isLastRoom){
+					Debug.Log("部屋を作成");
+					//部屋の作成
+					createRoom();
+				}
+
+
+			}
+		});
+	}
+
+	//部屋に入る
+	private void intoTheRoom(int roomnum){
+		//自分の情報をjson形式で保存
+		string json = "{\"name\":\"challenger\",\"rate\":1500}";
+		//部屋に自分の情報を反映
+		reference.Child("onlineroom").Child(roomnum.ToString()).Child("challengerid").SetRawJsonValueAsync(json);
+
+		//ゲームスタート
+		gameStart();
+	}
+
+	//部屋を作成
+	private void createRoom(){
+		int roomnum = PlayerPrefs.GetInt("roomnum") + 1;
+		PlayerPrefs.SetInt("roomnum",roomnum);
+
+		//自分の情報をjson形式で保存
+		string json = "{\"host\":\"hostman\",\"rate\":1500}";
+		//部屋に自分の情報を反映
+		reference.Child("onlineroom").Child(roomnum.ToString()).Child("hostid").SetRawJsonValueAsync(json);
+	}
+
+	//ゲームスタート
+	private void gameStart(){
+
+	}
+}
