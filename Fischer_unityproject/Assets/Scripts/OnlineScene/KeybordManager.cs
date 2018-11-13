7b647bb840944e95f8a5765a5278e3b5113740b6
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2018-11-14 03:33:28 +0900

    【機能追加】部屋を作成した側の処理

--- Fischer_unityproject/Assets/Scripts/OnlineScene/KeybordManager.cs
+++ Fischer_unityproject/Assets/Scripts/OnlineScene/KeybordManager.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 using UnityEngine.UI;
 using TMPro;
 
@@ -11,9 +14,41 @@ public class KeybordManager : MonoBehaviour {
 	public GameObject userCard; 
 	public GameObject imageBackground; //背景
 	public Sprite[] cardSprite = new Sprite[10]; //カードの画像
+	private DatabaseReference reference;
+	private DatabaseReference refRoom; //部屋までのref
+	private string roomnum_str = "";
 
 	private string callnum = "";
 	private int count = 0; //今何番目の数字か
+
+	private void Start(){
+		/*
+		 * Firebase関連
+		 */
+		// Set up the Editor before calling into the realtime database.
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://fischer-66e26.firebaseio.com");
+		// Get the root reference location of the database.
+		reference = FirebaseDatabase.DefaultInstance.RootReference;
+		Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+  		var dependencyStatus = task.Result;
+  		if (dependencyStatus == Firebase.DependencyStatus.Available) {
+		    // Create and hold a reference to your FirebaseApp, i.e.
+		    //   app = Firebase.FirebaseApp.DefaultInstance;
+		    // where app is a Firebase.FirebaseApp property of your application class.
+
+		    // Set a flag here indicating that Firebase is ready to use by your
+		    // application.
+		  } else {
+		    UnityEngine.Debug.LogError(System.String.Format(
+		      "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+		    // Firebase Unity SDK is not safe to use here.
+		  }
+		});
+	}
+
+	public void GetRef(string room){
+		roomnum_str = room;
+	}
 
 	public void OnClick(int num){
 		if(num==10){
@@ -34,7 +69,7 @@ public class KeybordManager : MonoBehaviour {
 	private void Call(){
 		if(count!=3)return;
 		//コールの処理
-		GameStart();
+		DecideSettingNum();
 	}
 
 	private void Reset(){
@@ -49,7 +84,56 @@ public class KeybordManager : MonoBehaviour {
 		textMain.SetActive(false);
 		textNumber.SetActive(true);
 		textNumber.GetComponent<TextMeshProUGUI>().text = callnum;
-	}	
+	}
+
+	//設定ナンバー決定
+	private void DecideSettingNum(){
+		Dictionary<string,object> number = new Dictionary<string, object>(){{"number",callnum}};
+		reference.Child("online").Child("room").Child(roomnum_str).
+			Child(PlayerPrefs.GetString("userid")).UpdateChildrenAsync(number);
+		
+		FirebaseDatabase.DefaultInstance
+			.GetReference("online")
+			.GetValueAsync().ContinueWith(task => {
+				if (task.IsFaulted) {
+				// Handle the error...
+				}
+				else if (task.IsCompleted) {
+					DataSnapshot snapshot = task.Result;
+					// Do something with snapshot...
+					if(snapshot.Child("room").Child(roomnum_str).
+						Child(PlayerPrefs.GetString("enemyid")).Child("number").Value == null){
+						//相手はまだ入力中
+						WaitEnemy();
+					}else{
+						//相手の入力が終了している
+						GameStart();
+					}
+				}
+			});
+	}
+
+	private void WaitEnemy(){
+		textMain.SetActive(true);
+		textNumber.SetActive(false);
+		textMain.GetComponent<Text>().text = "対戦相手が決めるまでもう少しお待ちください";
+		reference.Child("online").Child("room").Child(roomnum_str).
+			Child(PlayerPrefs.GetString("enemyid")).ChildAdded += HandleChildAdded;
+	}
+
+	void HandleChildAdded(object sender, ChildChangedEventArgs args) {
+      if (args.DatabaseError != null) {
+        Debug.LogError(args.DatabaseError.Message);
+        return;
+      }
+      // Do something with the data in args.Snapshot
+	  Debug.Log(args.Snapshot);
+	  if(args.Snapshot.Key == "number"){
+		  //敵のナンバー決定終了
+		  GameStart();
+	  }
+
+    }
 
 	private void GameStart(){
 		for(int i=0;i<3;i++){
@@ -74,4 +158,6 @@ public class KeybordManager : MonoBehaviour {
 		textMain.SetActive(true);
 		textMain.GetComponent<Text>().text = "Game Start!";
 	}
+
+
 }
--- Fischer_unityproject/Assets/Scripts/OnlineScene/MachingManager.cs
+++ Fischer_unityproject/Assets/Scripts/OnlineScene/MachingManager.cs
@@ -16,6 +16,10 @@ public class MachingManager : MonoBehaviour {
 	private GameObject indicator;
 	private DatabaseReference reference;
 	private bool isLastRoom = true;
+	private string userid = "";
+	private string roomnum_str = "";
+
+	
 
 	// Use this for initialization
 	void Start () {
@@ -42,11 +46,23 @@ public class MachingManager : MonoBehaviour {
 		  }
 		});
 
+		//初回起動時useridを設定
+		if(PlayerPrefs.GetString("userid") == ""){
+			System.Guid guid = System.Guid.NewGuid ();
+			string uuid = guid.ToString ();
+			PlayerPrefs.SetString("userid",uuid);
+		}else{
+			//Debug.Log(PlayerPrefs.GetString(PPKey.userid.ToString()));
+		}
+
 		//オブジェクトの参照
 		referenceObject();
 
 		//インジケーターの表示
 		indicator.GetComponent<ActivityIndicator>().Show();
+
+		//userid設定
+		userid = PlayerPrefs.GetString("userid");
 
 		//マッチング開始
 		searchRoom();
@@ -78,12 +94,29 @@ public class MachingManager : MonoBehaviour {
 					string name = (string)data.Child("name").Value;
 					int score   = (int)(long)data.Child("score").Value;
 					*/
-					if(data.ChildrenCount == 1){
-						//部屋に一人しかいない
-						intoTheRoom(int.Parse(data.Key));
-						Debug.Log(data.Key + "の部屋に参加");
-						isLastRoom = false;
-						break;
+					if(data.ChildrenCount == 1 ){
+						Dictionary<string,object> user = new Dictionary<string, object>();
+						user = data.Value as Dictionary<string,object>;
+						string key_userid = "";
+						foreach (string key in user.Keys) {
+							key_userid = key;
+						}
+						Debug.Log(key_userid);
+						if(key_userid != PlayerPrefs.GetString("userid")){
+							//部屋に一人しかいないかつその一人が自分じゃない
+							PlayerPrefs.SetString("enemyid",key_userid);
+							roomnum_str = data.Key;
+							intoTheRoom(int.Parse(data.Key));
+							Debug.Log(data.Key + "の部屋に参加");
+							isLastRoom = false;
+							break;
+						}else{
+							//部屋に一人しかいないけどその一人が自分
+							Debug.Log("自分が作った部屋に入りました");
+							waitMember(int.Parse(data.Key));
+							isLastRoom = false;
+							break;
+						}
 					}
 				}
 
@@ -103,7 +136,7 @@ public class MachingManager : MonoBehaviour {
 		//自分の情報をjson形式で保存
 		string json = "{\"name\":\"challenger\",\"rate\":1500}";
 		//部屋に自分の情報を反映
-		reference.Child("online").Child("room").Child(roomnum.ToString()).Child("challengerid").SetRawJsonValueAsync(json);
+		reference.Child("online").Child("room").Child(roomnum.ToString()).Child(userid).SetRawJsonValueAsync(json);
 
 		//ゲームスタート
 		gameStart();
@@ -123,22 +156,43 @@ public class MachingManager : MonoBehaviour {
 				//Firebaseから今ある部屋数を取得してそれに一足した部屋番号の部屋を作成
 				DataSnapshot snapshot = task.Result;
 				roomnum = int.Parse(snapshot.Child("roomnum").Value.ToString()) + 1;
+				roomnum_str = roomnum.ToString();
 				reference.Child("online").Child("roomnum").SetValueAsync(roomnum);
 
 				//自分の情報をjson形式で保存
 				string json = "{\"host\":\"hostman\",\"rate\":1500}";
 				//部屋に自分の情報を反映
 				reference.Child("online").Child("room").
-					Child(roomnum.ToString()).Child("hostid").SetRawJsonValueAsync(json);
+					Child(roomnum.ToString()).Child(userid).SetRawJsonValueAsync(json);
 				Debug.Log(roomnum+"の部屋を作成");
+				waitMember(roomnum);
 			}
 		});
-		waitMember();
 	}
 
-	private void waitMember(){
-		
+	private void waitMember(int roomnum){
+		Debug.Log("入ってくるのを待ってます");
+		var ref_online = FirebaseDatabase.DefaultInstance
+      		.GetReference("online");
+
+      	ref_online.Child("room").Child(roomnum.ToString()).ChildAdded += HandleChildAdded;
 	}
+
+	void HandleChildAdded(object sender, ChildChangedEventArgs args) {
+      if (args.DatabaseError != null) {
+        Debug.LogError(args.DatabaseError.Message);
+        return;
+      }
+      // Do something with the data in args.Snapshot
+	  Debug.Log(args.Snapshot.Key);
+	  if(args.Snapshot.Key != PlayerPrefs.GetString("userid")){
+		  //敵のidを保存
+		  PlayerPrefs.SetString("enemyid",args.Snapshot.Key);
+		  //誰かが入ってきた
+		  gameStart();
+	  }
+
+    }
 
 	//ゲームスタート
 	private void gameStart(){
@@ -150,5 +204,6 @@ public class MachingManager : MonoBehaviour {
 		textNumber.SetActive(false);
 		textMain.GetComponent<Text>().text = "プレイヤーが揃いました。\nナンバーをセットして対戦を開始してください。";
 		keybord.SetActive(true);
+		keybord.GetComponent<KeybordManager>().GetRef(roomnum_str);
 	}
 }
